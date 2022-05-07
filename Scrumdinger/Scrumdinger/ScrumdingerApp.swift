d6ccad9683c465641c85bfc09dbb161e3e9596c3
rkddnr330 <rkddnr330@gmail.com>
2022-05-07 19:18:28 +0900

    initial project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Park Kangwook on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
