b7d041efe734a28d436ad3fa225ab52802c9d9aa
rkddnr330 <rkddnr330@gmail.com>
2022-05-09 17:17:00 +0900

    [2/14] Views(1): Creating a Card View

---
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Park Kangwook on 2022/05/09.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String] //String으로 이루어진 List라는 의미를 '[String]'으로 표기
+    var lengthInMinutes: Int
+    var theme: Theme //Theme.swift에서 선언해준 enum 타입
+}
+
+//provide sample data
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
